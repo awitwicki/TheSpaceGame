9cb3c9889f3fd1cb84e2b5018e92c339e35dc576
Andrew Witwicki <vitvitskiy3@gmail.com>
2021-11-12 16:52:02 +0100

    Add meteors collision and developer mode

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 var myGamePiece;
 
+var debugMode = false;
 var meteors;
 
 function startGame() {
@@ -19,7 +20,7 @@ function startGame() {
     var _speed_y = Math.random() * 1 - 0.5;
 
     var _meteor = new component(50, 50, "res/meteor.png", _width, _height, _speed_x, _speed_y, "meteor");
-    
+
     meteors.push(_meteor);
   }
 }
@@ -28,7 +29,7 @@ var myGameArea = {
   canvas: document.createElement("canvas"),
   start: function () {
     document.body.innerWidth
-    this.canvas.width = window.innerWidth -2;
+    this.canvas.width = window.innerWidth - 2;
     this.canvas.height = window.innerHeight - 30;
     this.context = this.canvas.getContext("2d");
 
@@ -38,6 +39,11 @@ var myGameArea = {
     window.addEventListener('keydown', function (e) {
       myGameArea.keys = (myGameArea.keys || []);
       myGameArea.keys[e.keyCode] = (e.type == "keydown");
+
+      // Debug mode on/off
+      if (e.keyCode == 72) {
+        debugMode = !debugMode;
+      }
 
       // Change ship image
       this.isAnyKeyPressed = true;
@@ -81,6 +87,17 @@ function component(width, height, color, x, y, speed_x, speed_y, type) {
     ctx.translate(this.x, this.y);
     ctx.rotate(this.angle);
     ctx.drawImage(this.image, this.width / -2, this.height / -2, this.width, this.height);
+
+    if (debugMode) {
+      ctx.setTransform(1, 0, 0, 1, 0, 0);
+      ctx.beginPath();
+      ctx.moveTo(this.x, this.y);
+      ctx.lineTo(this.x + (this.speedX * 100), this.y + (this.speedY * 100));
+    }
+
+    ctx.strokeStyle = "#00FF00";
+    ctx.stroke();
+
     ctx.restore();
   }
 
@@ -111,12 +128,8 @@ function component(width, height, color, x, y, speed_x, speed_y, type) {
     this.y += this.speedY;
   }
 
-  this.newPosMeteor = function () {
-    this.averageX = Math.sin(this.angle) * this.trust;
-    this.averageY = -Math.cos(this.angle) * this.trust;
-    this.speedX += this.averageX;
-    this.speedY += this.averageY;
-
+  this.newPosMeteor = function (meteorIndex) {
+    // Collision with area
     if (this.y < 0 || this.y > this.gamearea.canvas.height) {
       this.speedY *= -1;
     }
@@ -124,9 +137,48 @@ function component(width, height, color, x, y, speed_x, speed_y, type) {
       this.speedX *= -1;
     }
 
+    // Find meteor with collision
+    var _collisionMeteor = findNearMeteor(meteorIndex);
+
+    // Change speed of meteor after collision with other meteors
+    if (_collisionMeteor != null) {
+
+      if (debugMode) {
+        ctx = myGameArea.context;
+        ctx.beginPath();
+        ctx.moveTo(this.x, this.y);
+        ctx.lineTo(_collisionMeteor.x, _collisionMeteor.y);
+        ctx.strokeStyle = "#FF0000";
+        ctx.stroke();
+      }
+
+      if (this.speedX * _collisionMeteor.speedX < 0) {
+        this.speedX *= -1;
+        _collisionMeteor.speedX *= -1;
+      } 
+      if (this.speedY * _collisionMeteor.speedY < 0) {
+        this.speedY *= -1;
+        _collisionMeteor.speedY *= -1;
+      }
+    }
+
     this.x += this.speedX;
     this.y += this.speedY;
   }
+}
+
+// Collision with other meteors that nearest above 50 between each other
+function findNearMeteor(meteorIndex) {
+  for (var i = 0; i < meteors.length; i++) {
+    if (i != meteorIndex) {
+      var _distance = Math.sqrt(Math.pow(meteors[i].x - (meteors[meteorIndex].x), 2) + Math.pow(meteors[i].y - meteors[meteorIndex].y, 2));
+
+      if (_distance < 50) {
+        return meteors[i];
+      }
+    }
+  }
+  return null;
 }
 
 // Update game tick
@@ -165,7 +217,7 @@ function updateGameArea() {
 
   // Update meteors
   for (var i = 0; i < meteors.length; i++) {
-    meteors[i].newPosMeteor();
     meteors[i].update();
+    meteors[i].newPosMeteor(i);
   }
 }
