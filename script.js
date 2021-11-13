224f5ae62276dc83fc0023645df99b7c713596a1
Andrew Witwicki <vitvitskiy3@gmail.com>
2021-11-13 21:56:04 +0100

    Add game ending

--- script.js
+++ script.js
@@ -1,22 +1,21 @@
-
-var myGamePiece;
+var ship;
 
 var debugMode = false;
 var meteors;
 
 function startGame() {
   myGameArea.start();
+  initGameLevel();
+}
 
-  // Create player ship
-  myGamePiece = new component(50, 50, "res/ship.png", 400, 400, 0, 0, "ship");
-
+function initGameLevel() {
   // Create meteors with random speed and location
   meteors = [];
 
   for (var i = 0; i < 50; i++) {
     var _speed_x = Math.random() * 1 - 0.5;
     var _speed_y = Math.random() * 1 - 0.5;
-    
+
     var _width = Math.random() * myGameArea.canvas.width;
     var _height = Math.random() * myGameArea.canvas.height;
     var _meteor = new component(50, 50, "res/meteor.png", _width, _height, _speed_x, _speed_y, "meteor");
@@ -28,6 +27,21 @@ function startGame() {
     }
     while (findNearMeteor(i))
   }
+
+  // Create player ship
+  ship = new component(50, 50, "res/ship.png", 400, 400, 0, 0, "ship");
+
+  ship.trust = 0;
+  ship.angle = 0;
+  ship.speedX = 0;
+  ship.speedY = 0;
+  ship.averageX = 0;
+  ship.averageY = 0;
+  do {
+    ship.x = Math.random() * myGameArea.canvas.width;
+    ship.y = Math.random() * myGameArea.canvas.height;
+  }
+  while (findNearMeteorAboveShip())
 }
 
 var myGameArea = {
@@ -52,14 +66,14 @@ var myGameArea = {
 
       // Change ship image
       this.isAnyKeyPressed = true;
-      myGamePiece.image.src = "res/ship_with_thrusters.png";
+      ship.image.src = "res/ship_with_thrusters.png";
     })
     window.addEventListener('keyup', function (e) {
       myGameArea.keys[e.keyCode] = (e.type == "keydown");
 
       // Change ship image
       if (!(myGameArea.keys[37] || myGameArea.keys[38] || myGameArea.keys[39] || myGameArea.keys[40])) {
-        myGamePiece.image.src = "res/ship.png";
+        ship.image.src = "res/ship.png";
       }
     })
   },
@@ -102,6 +116,18 @@ function component(width, height, color, x, y, speed_x, speed_y, type) {
       ctx.stroke();
     }
 
+    // Check collision with any meteors
+    if (this.type == "ship") {
+      var meteorCollision = findNearMeteorAboveShip();
+      if (meteorCollision) {
+        alert("Game over!");
+
+        // Reset game keyboard
+        myGameArea.keys = [];
+        initGameLevel();
+      }
+    }
+
     ctx.restore();
   }
 
@@ -111,9 +137,9 @@ function component(width, height, color, x, y, speed_x, speed_y, type) {
     this.speedX += this.averageX;
     this.speedY += this.averageY;
 
-    if (this.y > this.gamearea.canvas.height) {
+    if (this.y > myGameArea.canvas.height) {
       this.speedY = 0;
-      this.y = this.gamearea.canvas.height
+      this.y = myGameArea.canvas.height
     }
     if (this.x < 0) {
       this.speedX = 0;
@@ -123,9 +149,9 @@ function component(width, height, color, x, y, speed_x, speed_y, type) {
       this.speedY = 0;
       this.y = 0;
     }
-    if (this.x > this.gamearea.canvas.width) {
+    if (this.x > myGameArea.canvas.width) {
       this.speedX = 0;
-      this.x = this.gamearea.canvas.width;
+      this.x = myGameArea.canvas.width;
     }
 
     this.x += this.speedX;
@@ -200,39 +226,61 @@ function findNearMeteor(meteorIndex) {
   return null;
 }
 
+function findNearMeteorAboveShip() {
+  for (var i = 0; i < meteors.length; i++) {
+    var _distance = Math.sqrt(Math.pow(meteors[i].x - (ship.x), 2) + Math.pow(meteors[i].y - ship.y, 2));
+
+    // Draw lines to nearby meteors
+    if (debugMode && _distance < 300) {
+      ctx = myGameArea.context;
+      ctx.beginPath();
+      ctx.moveTo(ship.x, ship.y);
+      ctx.lineTo(meteors[i].x, meteors[i].y);
+      ctx.strokeStyle = "#FF0000";
+      ctx.stroke();
+    }
+
+    // Collision with meteor
+    if (_distance < 45) {
+      return true;
+    }
+  }
+  return false;
+}
+
 // Update game tick
 function updateGameArea() {
   myGameArea.clear();
-  myGamePiece.trust = 0;
-  myGamePiece.speedX *= 0.99;
-  myGamePiece.speedY *= 0.99;
+  ship.trust = 0;
+  ship.speedX *= 0.99;
+  ship.speedY *= 0.99;
 
   // If any control key is pressed
   if (myGameArea.keys) {
     // Thruster left (idk maybe right)
     if (myGameArea.keys[37]) {
-      myGamePiece.angle -= 3 * Math.PI / 180;
+      ship.angle -= 3 * Math.PI / 180;
     }
 
     // Thruster right (idk maybe left)
     if (myGameArea.keys[39]) {
-      myGamePiece.angle += 3 * Math.PI / 180;
+      ship.angle += 3 * Math.PI / 180;
     }
 
     // Thruster forward
     if (myGameArea.keys[38]) {
-      myGamePiece.trust = 0.1;
+      ship.trust = 0.1;
     }
 
     // Thruster backward
     if (myGameArea.keys[40]) {
-      myGamePiece.trust = -0.1;
+      ship.trust = -0.1;
     }
   }
 
   // Update player ship
-  myGamePiece.newPos();
-  myGamePiece.update();
+  ship.newPos();
+  ship.update();
 
   // Update meteors
   for (var i = 0; i < meteors.length; i++) {
